3ef08a9434a1f9ab834f877180db9c1ace151018
André Angeloni <andrefangeloni@gmail.com>
2021-03-28 22:21:00 -0300

    feat: countdown working

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,24 +2,51 @@ import React from 'react';
 
 import styles from '../styles/components/Countdown.module.css';
 
-const Countdown = () => (
-  <div>
-    <div className={styles.countdownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
-      </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
-    </div>
+const Countdown = () => {
+  const [time, setTime] = React.useState(25 * 60);
+  const [active, setActive] = React.useState(false);
 
-    <button type="button" className={styles.countdownButton}>
-      Iniciar um ciclo
-    </button>
-  </div>
-);
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  const startCountdown = () => {
+    setActive(true);
+  };
+
+  React.useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time]);
+
+  return (
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </div>
+
+      <button
+        type="button"
+        onClick={() => startCountdown()}
+        className={styles.countdownButton}
+      >
+        Iniciar um ciclo
+      </button>
+    </div>
+  );
+};
 
 export default Countdown;
